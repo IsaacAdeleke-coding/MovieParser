3d01cfdfc6e6c86baf9ac9aa861c98543082f81a
Isaac Adeleke <158352924+IsaacAdeleke-coding@users.noreply.github.com>
2024-11-10 14:26:49 -0800

    Initial commit: Added MovieParser project with main.cpp, MovieParser.hpp, and MovieParser.cpp
    - Created Movie struct to hold title, season, and year data.
    - Implemented parseMovieString function to parse a formatted string into a Movie struct.
    - Added main.cpp for testing the parsing function.
    - Organized code into separate header and implementation files.

---
+++ MovieParser/MovieParser.cpp
@@ -0,0 +1,8 @@
+//
+//  MovieParser.cpp
+//  MovieParser
+//
+//  Created by Isaac Adeleke on 11/10/24.
+//
+
+#include "MovieParser.hpp"
---
+++ MovieParser/MovieParser.hpp
@@ -0,0 +1,13 @@
+//
+//  MovieParser.hpp
+//  MovieParser
+//
+//  Created by Isaac Adeleke on 11/10/24.
+//
+
+#ifndef MovieParser_hpp
+#define MovieParser_hpp
+
+#include <stdio.h>
+
+#endif /* MovieParser_hpp */
